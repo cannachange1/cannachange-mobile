5ab8584aa5ca784ba9d15380758ffcc30e3f156a
Varsik Nikoyan <62645936+Nikoyan-V@users.noreply.github.com>
2022-02-03 15:52:12 +0400

    avatar widget changed

--- lib/ui/pages/main_navigation_consumer/consumer/consumer_home_page.dart
+++ lib/ui/pages/main_navigation_consumer/consumer/consumer_home_page.dart
@@ -3,6 +3,7 @@ import 'package:cannachange/store/personal_data_state/personal_data_state.dart';
 import 'package:cannachange/ui/widgets/avatar_widget.dart';
 import 'package:cannachange/values/values.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:get_it/get_it.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -26,110 +27,143 @@ class _ConsumerHomePageState extends State<ConsumerHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              const SizedBox(
+                height: 40,
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const AvatarWidget(isDispensary: false,),
+                  const AvatarWidget(
+                    isDispensary: false,
+                  ),
                   QrImage(
                     data: personalDataState.clientModel!.qrCode!,
                     version: QrVersions.auto,
-                    size: 120,
-                  ),
-                ],
-              ),
-              const Text(
-                'Points Earned',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: AppColors.darkGrey,
-                  fontWeight: FontWeight.w600,
-                ),
-              ),
-              ListView.builder(
-                itemCount: 3,
-                shrinkWrap: true,
-                itemBuilder: (context, position) {
-                  return Card(
-                    elevation: 8,
-                    color: AppColors.secondAccent,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'name',
-                            style: TextStyle(fontSize: 22.0, color: AppColors.lightGrayColor),
-                          ),
-                          Stack(
-                            children: const [
-                              Icon(
-                                Icons.filter_none,
-                                size: 50,
-                                color: AppColors.lightGrayColor,
-                              ),
-                              Positioned(
-                                child: Text(
-                                  '3',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w800,
-                                      fontSize: 24,
-                                      color: AppColors.darkGrey),
-                                ),
-                                right: 12,
-                                top: 6,
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-              Row(
-                children: [
-                  const Text(
-                    'Dispensary Address: ',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: AppColors.darkGrey,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Text(
-                    personalDataState.dispensaryModel!.address1 ?? '',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: AppColors.secondAccent,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: [
-                  const Text(
-                    'Dispensary Hours:',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: AppColors.darkGrey,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Text(
-                    '${personalDataState.dispensaryModel!.startHour} - ${personalDataState.dispensaryModel!.endHour}',
-                    style: const TextStyle(
-                      fontSize: 16,
-                      color: AppColors.secondAccent,
-                      fontWeight: FontWeight.w600,
-                    ),
+                    size: 140,
                   ),
                 ],
               ),
               const SizedBox(
                 height: 40,
               ),
+              const Text(
+                'Points Earned',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: AppColors.darkGrey,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Divider(
+                color: AppColors.secondAccent,
+                indent: 2,
+                thickness: 3,
+              ),
+              const Divider(
+                thickness: 1,
+                height: 0,
+              ),
+              Observer(
+                builder: (_) => ListView.builder(
+                  itemCount: personalDataState.consumerPointList.length,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) {
+                    return personalDataState.consumerPointList.isNotEmpty
+                        ? Card(
+                            elevation: 8,
+                            color: AppColors.secondAccent,
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 8, horizontal: 20),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    personalDataState.consumerPointList[index]
+                                        .dispensary!.name!,
+                                    style: const TextStyle(
+                                        fontSize: 22.0,
+                                        color: AppColors.lightGrayColor),
+                                  ),
+                                  Stack(
+                                    children: [
+                                      const Icon(
+                                        Icons.filter_none,
+                                        size: 50,
+                                        color: AppColors.lightGrayColor,
+                                      ),
+                                      Positioned(
+                                        child: Text(
+                                          personalDataState
+                                              .consumerPointList[index].point!
+                                              .toString(),
+                                          style: const TextStyle(
+                                              fontWeight: FontWeight.w800,
+                                              fontSize: 24,
+                                              color: AppColors.darkGrey),
+                                        ),
+                                        right: 12,
+                                        top: 6,
+                                      )
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        : const Text(
+                            "You didn't earn points yet",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18,
+                                color: AppColors.darkGrey),
+                          );
+                  },
+                ),
+              ),
+              const Text(
+                'Waste Saved',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: AppColors.darkGrey,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const Divider(
+                color: AppColors.secondAccent,
+                indent: 2,
+                thickness: 3,
+              ),
+              const Divider(
+                thickness: 1,
+                height: 0,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Text(
+                    personalDataState.clientModel!.wasteSaved.toString(),
+                    style: TextStyle(
+                      fontSize: 30,
+                      color: AppColors.darkGrey.withOpacity(.7),
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  const Icon(
+                    Icons.scale,
+                    color: AppColors.secondAccent,
+                    size: 50,
+                  ),
+                ],
+              )
             ],
           ),
         ),
